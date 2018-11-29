4d5b1c0035ca42c00fa5dc87c11ae7feef8ca92f
Jim Shaw <jimxshaw@gmail.com>
2018-11-29 13:24:33 -0600

    Initial commit.

---
+++ src/main/java/me/jimmyshaw/animalfacts/AnimalfactsApplication.java
@@ -0,0 +1,12 @@
+package me.jimmyshaw.animalfacts;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnimalfactsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AnimalfactsApplication.class, args);
+	}
+}
---
+++ src/test/java/me/jimmyshaw/animalfacts/AnimalfactsApplicationTests.java
@@ -0,0 +1,16 @@
+package me.jimmyshaw.animalfacts;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class AnimalfactsApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
