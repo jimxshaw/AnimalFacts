6b1987f65ea64d93e0a7cdc4ca19b5753a994f97
Jim Shaw <jimxshaw@gmail.com>
2018-11-30 13:59:33 -0600

    Refactor AnimalFactsRepository from class into interface.

--- src/main/java/me/jimmyshaw/animalfacts/AnimalFactsRepository.java
+++ src/main/java/me/jimmyshaw/animalfacts/AnimalFactsRepository.java
@@ -1,41 +1,8 @@
 package me.jimmyshaw.animalfacts;
 
-import org.springframework.stereotype.Component;
+import org.springframework.data.repository.CrudRepository;
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
-import java.util.Optional;
-
-@Component
-public class AnimalFactsRepository {
-    private final List<AnimalFact> facts = new ArrayList<>();
-
-    public AnimalFactsRepository() {
-        facts.add(createFact(1L, "Prairie dogs greet one another by kissing"));
-        facts.add(createFact(2L, "Anteaters don't have teeth"));
-    }
-
-    private AnimalFact createFact(final Long id, final String fact) {
-        final AnimalFact animalFact = new AnimalFact();
-        animalFact.setId(id);
-        animalFact.setFact(fact);
-
-        return animalFact;
-    }
-
-    public Iterable<AnimalFact> findAll() {
-        return Collections.unmodifiableList(facts);
-    }
-
-    public Optional<AnimalFact> findById(final Long id) {
-        for (final AnimalFact fact : facts) {
-            if (id.equals(fact.getId())) {
-                return Optional.of(fact);
-            }
-        }
-
-        return Optional.empty();
-    }
-
+public interface AnimalFactsRepository extends CrudRepository<AnimalFact, Long> {
+    
 }
+
