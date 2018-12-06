66a2380bdfcb96ad593f8346a8573e2f776e60f4
Jim Shaw <jimxshaw@gmail.com>
2018-12-06 14:37:22 -0600

    Add seed data sql file.

---
+++ src/main/resources/db/migration/V2__seed-data.sql
@@ -0,0 +1,10 @@
+insert into animal_fact (id, fact) values (nextval('hibernate_sequence'), 'Elephants are the only animal that cannot jump');
+insert into animal_fact (id, fact) values (nextval('hibernate_sequence'), 'Slugs have four noses');
+insert into animal_fact (id, fact) values (nextval('hibernate_sequence'), 'It takes a sloth two weeks to digest its food');
+insert into animal_fact (id, fact) values (nextval('hibernate_sequence'), 'The heart of a shrimp is located in its head');
+insert into animal_fact (id, fact) values (nextval('hibernate_sequence'), 'The fingerprints of a koala are so indistinguishable from humans that they have on occasion been confused at a crime scene');
+insert into animal_fact (id, fact) values (nextval('hibernate_sequence'), 'Nearly three percent of the ice in Antarctic glaciers is penguin urine');
+insert into animal_fact (id, fact) values (nextval('hibernate_sequence'), 'It is possible to hypnotize a frog by placing it on its back and gently stroking its stomach');
+insert into animal_fact (id, fact) values (nextval('hibernate_sequence'), 'A snail can sleep for three years');
+insert into animal_fact (id, fact) values (nextval('hibernate_sequence'), 'Bats always turn left when leaving a cave');
+insert into animal_fact (id, fact) values (nextval('hibernate_sequence'), 'A cow gives nearly 200,000 glasses of milk in a lifetime');
