835bfd180685dc505f09bb7124bfd18e2d74e85b
Jim Shaw <jimxshaw@gmail.com>
2018-12-07 10:49:58 -0600

    Add repo to index controller index method.

--- src/main/java/me/jimmyshaw/animalfacts/AnimalFactsIndexController.java
+++ src/main/java/me/jimmyshaw/animalfacts/AnimalFactsIndexController.java
@@ -1,14 +1,26 @@
 package me.jimmyshaw.animalfacts;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+import java.util.Map;
+
 @Controller
 public class AnimalFactsIndexController {
 
+    private final AnimalFactsRepository repository;
+
+    @Autowired
+    public AnimalFactsIndexController(final AnimalFactsRepository repository) {
+        this.repository = repository;
+    }
+
     @RequestMapping(value = "/", method = RequestMethod.GET)
-    public String index() {
+    public String index(final Map<String, Object> model) {
+        model.put("facts", repository.findAll());
+
         return "index";
     }
 
